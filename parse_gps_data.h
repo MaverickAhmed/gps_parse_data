5aeb91ba85890d90e4d7983108949b075d281d7a
maverickahmed <ahmedraza24201@gmail.com>
2023-05-27 15:02:08 +0500

    adding validation feature

--- parse_gps_data.h
+++ parse_gps_data.h
@@ -5,5 +5,8 @@ typedef struct {
     char params[10][20]; //defining a 2-dim array that can store upto 10 parameters
 } GPSData;
 
+bool checksum_validation(const char* packet, GPSData* gpsData);
+void extract_func(const char* packet, GPSData* gpsData);
 void parse_gps_data(const char* packet, GPSData* gpsData);
+
 #endif  // PARSE_GPS_DATA_H
