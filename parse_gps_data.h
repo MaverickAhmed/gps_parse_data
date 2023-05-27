b9797a4ddaf07c35772f79c7606e38ddb5346df6
maverickahmed <ahmedraza24201@gmail.com>
2023-05-27 15:12:06 +0500

    adding gga_verification function prototype

--- parse_gps_data.h
+++ parse_gps_data.h
@@ -5,8 +5,9 @@ typedef struct {
     char params[10][20]; //defining a 2-dim array that can store upto 10 parameters
 } GPSData;
 
+bool gga_validation(const char* packet, GPSData* gpsData);
 bool checksum_validation(const char* packet, GPSData* gpsData);
 void extract_func(const char* packet, GPSData* gpsData);
-void parse_gps_data(const char* packet, GPSData* gpsData);
+int parse_gps_data(const char* packet, GPSData* gpsData);
 
 #endif  // PARSE_GPS_DATA_H
