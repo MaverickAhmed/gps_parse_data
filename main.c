fc4435c5d23e5f2fe63614c85c8ffc79074306d4
maverickahmed <ahmedraza24201@gmail.com>
2023-05-27 14:53:02 +0500

    creating base structure

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "parse_gps_data.h"
+
+int main() {
+    // Given GGA packet 
+    const char* packet1 = "$GPGGA,002153.000,3342.6618,N,11751.3858,W,1,10,1.2,27.0,M,-34.2,M,,0000*5E";
+    return 0;
+}
