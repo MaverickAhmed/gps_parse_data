cb60a48991e8c906129f0f208993403252785153
maverickahmed <ahmedraza24201@gmail.com>
2023-05-27 15:10:59 +0500

    implementation of validation checks

--- parse_gps_data.c
+++ parse_gps_data.c
@@ -2,6 +2,16 @@
 #include <string.h>
 #include <stdlib.h>
 
+bool gga_validation(const char* packet, GPSData* gpsData)
+{
+    if (packet == NULL || gpsData == NULL || strncmp(packet, "$GPGGA,", 7) != 0) {
+        // Null packet or not a GGA packet
+        printf("INVALID FORMAT (GGA FORMAT NOT FOUND)!\n");
+        return false;
+    }
+    return true;
+}
+
 bool checksum_validation(const char* packet, GPSData* gpsData) {
     int i = 1;                               // Start after '$' symbol
     unsigned char cal_checksum = 0;          // Using unsigned char for bitwise operations
@@ -15,4 +25,10 @@ bool checksum_validation(const char* packet, GPSData* gpsData) {
     unsigned char checksum = strtol(packet_checksum, &endptr, 16);
 
     return (cal_checksum == checksum);
+}
+
+int parse_gps_data(const char* packet, GPSData* gpsData)
+{
+    if (!gga_validation(packet, gpsData)) return 0;
+    checksum_validation(packet, gpsData) ? extract_func(packet, gpsData) : printf("GPS LOCK FAILURE!\n");
 }
